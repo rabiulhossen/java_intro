44aba4277352a7a76b6f5f5710933d2e0adde7b3
Rabiul Hossen <rhsimon69@gmail.com>
2024-02-17 23:57:09 +0600

    initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+
+//public us used for accessing the class
+// void used for which return type of the value
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+  public static void main(String[] args) {
+ 
+Scanner input = new Scanner(System.in);
+    // scanner used for read input from user
+    double area,length,breadth;
+  
+    length = input.nextDouble();
+    breadth = input.nextDouble();
+    // read double input from user
+    area = length*breadth;
+    System.out.println("Enter the length of the rectangle" + area);
+  }
+}
+
+
+ 
+
+
+
